4c2ab217894bbda12aceb672e0bc11d2ddcbcd41
indri yani (2035036) <100121360+indriYani77@users.noreply.github.com>
2022-04-08 10:55:37 +0700

    Add files via upload

---
+++ website/contact.php
@@ -0,0 +1,19 @@
+<div class="page">
+    <h2>Hubungi Kami</h2>
+    <p>Telp Unbara: 0735-326122
+
+        <br>WA yang bersangkutan: 
+        <br>082125547849 (Indri Yani)
+        <br>082278184806 (Alvin)
+        <br>082280202387 (Bayu)
+        <br>082280486277 (Ridho)
+        <br>081278073426 (Agil)
+        <br>082186297408 (Dewy)
+        <br>Hari Senin - Minggu
+   Jam 08.00 - 20.00 WIB</p>
+ 
+    <p>Jl. Ratu Pengulu
+        No 2301, Karang Sari, Baturaja, Tanjung Baru, Baturaja Timur, Ogan Komering Ulu
+        Sumatera Selatan
+    32115</p>
+</div>
---
+++ website/home.php
@@ -0,0 +1,5 @@
+<div class="page">
+    <h2>Website informasi tentang PPMB Universitas Baturaja</h2>
+    <p> Pendidikan berkarakter berawal dari kampus yang berkarakter, yaitu universitas baturaja. Mari untuk teman-teman yang belum bergabung segera bergabung di universitas baturaja dan Semua info PPMB lengkap ada di sini, silahkan anda hubungi nomor yang tertera di tab contact.</p>
+    <p>Jika anda ingin melihat informasi atau profil lebih lanjut tetang Universitas Baturaja, silahkan anda copy link website Universitas Baturaja berikut 'https//www.unbara.ac.id'.</p>
+</div>
